2c3b07602f7c58a92c994b3d45dcf7d19fc3d2ee
khlee8688 <hosanna62100@gmail.com>
2024-12-02 22:39:48 +0900

    Feat: 텍스트 문장 단위로 끊어서 world space에 rendering

--- Assets/Scripts/KH/BackScript.cs
+++ Assets/Scripts/KH/BackScript.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class BackScript : MonoBehaviour
 {
+    [SerializeField] TMP_Text text;
+    public string script;
+    private int scriptIndex = 0;
+    private string[] sentences;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sentences = script.Split('.');
+        text.text = sentences[scriptIndex];
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void NextString(){
+        scriptIndex++;
+        if(scriptIndex < sentences.Length){
+            text.text = sentences[scriptIndex];
+        }
+        else{
+            StringEnd();
+        }
+    }
+
+    void StringEnd(){
+
     }
 }
