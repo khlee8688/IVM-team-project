fec4586f92a2dc9a0e6c7f353262f431794b4962
Jangmyun <cndlsrb2739@gmail.com>
2024-12-20 05:24:28 +0900

    [feat #14] 사운드 매니저 스크립트 작성

--- Assets/Scripts/Chu/SoundManager.cs
+++ Assets/Scripts/Chu/SoundManager.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Timeline.Actions;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    public AudioClip soundToPlay;
+    private AudioSource audioSource;
+    public static SoundManager Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    public void PlaySound(AudioClip audioClip)
+    {
+        if (audioClip != null && audioSource != null)
+        {
+            audioSource.PlayOneShot(audioClip);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
